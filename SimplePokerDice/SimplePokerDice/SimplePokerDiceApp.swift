097c39e81f3ec27dac92d3b1a00a354cbb02e141
AD <silentflute.ad@gmail.com>
2025-04-22 20:51:23 +0100

    Initial project creation

---
+++ SimplePokerDice/SimplePokerDice/SimplePokerDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePokerDiceApp.swift
+//  SimplePokerDice
+//
+//  Created by Amarjit on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimplePokerDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
