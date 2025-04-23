2c7176955acb3bcc1bab9a69e04e46b2d7ac42cc
AD <silentflute.ad@gmail.com>
2025-04-23 09:00:38 +0100

    remove comments

--- SimplePokerDice/SimplePokerDice/ContentView.swift
+++ SimplePokerDice/SimplePokerDice/ContentView.swift
@@ -8,19 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    /*
-    @State private var currentDie = Int.random(in: 0...6)
-
-    var body: some View {
-        VStack {
-            DiceFaceView(index: currentDie)
-                .frame(width: 100, height: 100)
-
-            Button("Roll") {
-                currentDie = Int.random(in: 1...6)
-            }
-        }
-    }*/
     @State private var gameState = DiceGameState()
     var body: some View {
             VStack(spacing: 20) {
